0f8a5754a2d3aa2f94c379872177168eabe11c1a
Софья <zakharchuk.sofya@mail.ru>
2024-02-10 00:12:12 +0400

    C4

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('accounts')
 export class AccountsController {
@@ -16,6 +17,7 @@ export class AccountsController {
 
     // @ApiOperation({summary: 'Получение всех пользователей'})
     // @ApiResponse({status: 200, type: [Account]})
+    @UseGuards(AuthGuard)
     @Get()
     getAcc(){
         return this.accountsService.getAllAccounts();
